4593f4f7e239ac3ebf4c3996312c4bf36e4a1211
Roy Quilor <royquilor@gmail.com>
2025-01-24 10:51:24 +0000

    fix: add proper SVG types to DotPattern component

--- components/ui/dot-pattern.tsx
+++ components/ui/dot-pattern.tsx
@@ -1,18 +1,17 @@
 import { useId } from "react";
-
 import { cn } from "@/lib/utils";
 
-interface DotPatternProps {
-  width?: any;
-  height?: any;
-  x?: any;
-  y?: any;
-  cx?: any;
-  cy?: any;
-  cr?: any;
+interface DotPatternProps extends React.SVGAttributes<SVGSVGElement> {
+  width?: number;
+  height?: number;
+  x?: number;
+  y?: number;
+  cx?: number;
+  cy?: number;
+  cr?: number;
   className?: string;
-  [key: string]: any;
 }
+
 export function DotPattern({
   width = 16,
   height = 16,
